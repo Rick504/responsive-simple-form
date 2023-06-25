ae4e3bbd689fd1eb50c61c8f9e7acb2b8ffa1b3c
Rick Wesley <rick.wes1@gmail.com>
2023-06-25 14:11:45 -0300

    create validaty fields inputs

--- script.js
+++ script.js
@@ -1,16 +1,49 @@
+function validityField(parentElementId, text) {
+  const parentElement = document.getElementById(parentElementId);
+  const inp = parentElement.querySelector('input');
+  const errorElement = parentElement.querySelector('.error');
+
+  if (inp.value.trim() === '') {
+    if (!errorElement) {
+      const createElementError = `<div class="error">${text}</div>`;
+      parentElement.insertAdjacentHTML('beforeend', createElementError);
+
+      inp.style.border = '1px solid red';
+      parentElement.style.color = 'red';
+    }
+    return false;
+  }
+
+  parentElement.style.color = '';
+  inp.style.border = '';
+  errorElement ? errorElement.remove() : '';
+  return true;
+}
+
 const form = document.querySelector('#form-register');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 
-  data = {
-    name: e.target.elements['user-name'].value,
-    age: e.target.elements['user-age'].value,
-    city: e.target.elements['user-city'].value,
-    street: e.target.elements['user-street'].value,
-    number: e.target.elements['user-number'].value,
-    complement: e.target.elements['user-complement'].value,
-  };
+  const valids = [
+    validityField('info-name', 'Campo nome precisa ser preenchido.'),
+    validityField('info-age', 'Campo idade precisa ser preenchido.'),
+    validityField('info-city', 'Campo cidade precisa ser preenchido.'),
+    validityField('info-street', 'Campo rua precisa ser preenchido.'),
+  ];
 
-  console.log(data);
+  const allValid = valids.every((result) => result === true);
+
+  if (allValid) {
+    data = {
+      name: e.target.elements['user-name'].value.trim(),
+      age: e.target.elements['user-age'].value.trim(),
+      city: e.target.elements['user-city'].value.trim(),
+      street: e.target.elements['user-street'].value.trim(),
+      number: e.target.elements['user-number'].value.trim(),
+      complement: e.target.elements['user-complement'].value.trim(),
+    };
+
+    console.log(data);
+  }
 });
